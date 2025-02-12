a83f9d8a4ef933151cb8253b5b3e404a7b85ab88
ColinKnox <colinwknox@gmail.com>
2025-02-11 20:16:50 -0500

    Made a small project based on the lessons about functions from the Boot.dev Python course.

---
+++ intro.py
@@ -0,0 +1,6 @@
+# Function
+def neuromancer_opening(setting, tv_color, tv_description):
+    print(f"The sky above the {setting} was the color of {tv_color}, tuned to a {tv_description} channel.")
+
+# TEST
+neuromancer_opening("racetrack", "raw fire opal", "buzzing")
